79f88492101c785dab644d192f202abd077a8bc9
JUAN WILLIAN DA CONCEICAO ARAUJO <200032797@ucsal.br>
2025-08-21 09:13:56 -0300

    refactor: mudando classe de serviço para calcular somente o total

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		System.setProperty("tax.rate", "0.1");
 		Order o = new Order();
 		o.add(new Item("Café", 8.0, 2)); // 16
 		o.add(new Item("Bolo", 12.5, 1)); // 12.5 -> subtotal 28.5
