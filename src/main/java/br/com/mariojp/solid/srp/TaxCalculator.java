338a99c0a198b1af8fbddf8fcb45bff6d6d59274
JuanWillian <juanwcaraujo28@gmail.com>
2025-08-22 10:36:42 -0300

    fix: corrigindo caso o valor do System.getPropety venha nulo

--- src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
+++ src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
@@ -3,8 +3,12 @@ package br.com.mariojp.solid.srp;
 public class TaxCalculator {
 
 	public double calcularImposto(double subtotal) {
-		double tax = Double.parseDouble(System.getProperty("tax.rate")) * subtotal;
-		return tax;
+       double tax;
+        if(System.getProperty("tax.rate").isEmpty()){
+            return tax = 0.1 * subtotal;
+        }
+
+		return tax = Double.parseDouble(System.getProperty("tax.rate")) * subtotal;
 	}
 
 }
